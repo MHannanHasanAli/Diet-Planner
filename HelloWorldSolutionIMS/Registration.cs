05a59d3f19b1b7c0ae296c73dc54a10716cd741f
Muhammad Hannan Hasan Ali <muhammadhannanhasanali@outlook.com>
2023-11-05 18:02:04 +0500

    done

--- HelloWorldSolutionIMS/Registration.cs
+++ HelloWorldSolutionIMS/Registration.cs
@@ -185,7 +185,7 @@ namespace HelloWorldSolutionIMS
             fileno_new = fileno_new + 1;
             if (edit == 0)
             {
-                if (firstname.Text != "" || familyname.Text != "" || gender.Text != "" || mobileno.Text != "" || dob.Text != "" )
+                if (firstname.Text != "" && familyname.Text != "" && gender.Text != "" && mobileno.Text != "" || dob.Text != "")
                 {
                     try
                     {
@@ -311,12 +311,14 @@ namespace HelloWorldSolutionIMS
         }
         private void Registration_Load(object sender, EventArgs e)
         {
+            MainClass.HideAllTabsOnTabControl(tabControl1);
             int fileno_new = GetLastFileno();
             fileno_new = fileno_new + 1;
 
             fileno.Text = fileno_new.ToString();
 
             subscriptionstatus.Text = "No";
+            
             ShowCustomer(guna2DataGridView1, IDDGV, FILENODGV, firstnamedgv, familynamedgv, subscriptionstartdatedgv, subscriptionenddatedgv, nutritionistnamedgv);
 
         }
@@ -512,6 +514,336 @@ namespace HelloWorldSolutionIMS
             {
                 e.Handled = true; // Ignore the keypress if it's not a number or a control character
             }
+        }
+
+        private void bodycomp_Click(object sender, EventArgs e)
+        {
+            if(fileno.Text != "")
+            {
+               
+                int value = int.Parse(fileno.Text);
+
+                try
+                {
+                    if (MainClass.con.State != ConnectionState.Open)
+                    {
+                        MainClass.con.Open();
+                        conn = 1;
+                    }
+
+                    SqlCommand cmd2 = new SqlCommand("SELECT  FIRSTNAME, FAMILYNAME, MOBILENO, GENDER, AGE, NutritionistName FROM CUSTOMER " +
+                        "WHERE FILENO = @fileno", MainClass.con);
+
+                    cmd2.Parameters.AddWithValue("@fileno", value);
+                    SqlDataReader reader2 = cmd2.ExecuteReader();
+                    if (reader2.Read())
+                    {
+                        
+                        filenobmi.Text      = fileno.Text;
+                        firstnamebmi.Text   = reader2["FIRSTNAME"].ToString();
+                        familynamebmi.Text  = reader2["FAMILYNAME"].ToString();
+                        mobilenobmi.Text    = reader2["MOBILENO"].ToString();
+                        genderbmi.Text      = reader2["GENDER"].ToString();
+                        agebmi.Text         = reader2["AGE"].ToString();
+                        nutritionistbmi.Text = reader2["NutritionistName"].ToString();
+                        tabControl1.SelectedIndex = 1;
+                        filenobmi.ReadOnly = true; 
+                        firstnamebmi.ReadOnly = true;
+                        familynamebmi.ReadOnly = true;
+                        mobilenobmi.ReadOnly = true;
+                        genderbmi.DropDownStyle = ComboBoxStyle.DropDownList;
+                        agebmi.ReadOnly = true;
+                        nutritionistbmi.ReadOnly = true;
+                    }
+                    else
+                    {
+                        
+                        MessageBox.Show("No customer with this file no exist!");
+                    }
+
+                    reader2.Close();
+                    if (conn == 1)
+                    {
+                        MainClass.con.Close();
+                        conn = 0;
+                    }
+                    ShowBodyComposition(guna2DataGridView2, idbc, datebc, bcabc, heightbc, weightbc, agebc, fatsbc, proteinbc, waterbc, mineralsbc, visceralfatsbc, abdominalfatsbc, bmibc, bmrbc);
+
+                }
+                catch (Exception ex)
+                {
+                 
+                    MainClass.con.Close();
+                    MessageBox.Show(ex.Message);
+                   
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please feed file no first!");
+            }
+        }
+        private void floatlock(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true; // Ignore the keypress if it's not a number, a control character, or a decimal point
+            }
+
+            // Allow only one decimal point
+            Guna.UI2.WinForms.Guna2TextBox textBox = (Guna.UI2.WinForms.Guna2TextBox)sender;
+            if (e.KeyChar == '.' && textBox.Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+        }
+        private void AddBMI_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 2;
+        }
+
+        private void SaveBMI_Click(object sender, EventArgs e)
+        {
+            
+            if (edit == 0)
+            {
+                if (firstnamebmi.Text != "" && familynamebmi.Text != "" && genderbmi.Text != "" && mobilenobmi.Text != "" || filenobmi.Text != "")
+                {
+                    try
+                    {
+                        MainClass.con.Open();
+                        SqlCommand cmd = new SqlCommand("INSERT INTO BodyComposition (DATE, BCA, LENGTH, WEIGHT, AGE, FATS, PROTEIN, WATER, MINERALS, VISCERAL_FATS, ABDOMINAL_FAT, BMI, BMR, CustomerID) " +
+                                                        "VALUES (@DATE, @BCA, @LENGTH, @WEIGHT, @AGE, @FATS, @PROTEIN, @WATER, @MINERALS, @VISCERAL_FATS, @ABDOMINAL_FAT, @BMI, @BMR, @CustomerID)", MainClass.con);
+
+                        // Assuming you have a customer ID, replace customerIDValue with the actual ID.
+                        int customerIDValue = int.Parse(filenobmi.Text); // Get the actual customer ID from your application logic.
+
+                        cmd.Parameters.AddWithValue("@DATE", DateTime.Now); // Assuming the current date is used.
+                        cmd.Parameters.AddWithValue("@BCA", bca.Text); // Replace bcaValue with the actual value.
+                        cmd.Parameters.AddWithValue("@LENGTH", height.Text); // Replace lengthValue with the actual value.
+                        cmd.Parameters.AddWithValue("@WEIGHT", weight.Text); // Replace weightValue with the actual value.
+                        cmd.Parameters.AddWithValue("@AGE", agebmi.Text); // Replace ageValue with the actual value.
+                        cmd.Parameters.AddWithValue("@FATS", fats.Text); // Replace fatsValue with the actual value.
+                        cmd.Parameters.AddWithValue("@PROTEIN", protein.Text); // Replace proteinValue with the actual value.
+                        cmd.Parameters.AddWithValue("@WATER", water.Text); // Replace waterValue with the actual value.
+                        cmd.Parameters.AddWithValue("@MINERALS", minerals.Text); // Replace mineralsValue with the actual value.
+                        cmd.Parameters.AddWithValue("@VISCERAL_FATS", visceralfats.Text); // Replace visceralFatsValue with the actual value.
+                        cmd.Parameters.AddWithValue("@ABDOMINAL_FAT", abdominalfats.Text); // Replace abdominalFatValue with the actual value.
+
+                        var genderval = genderbmi.Text;
+                        var heightval = double.Parse(height.Text);
+                        var heightforBMR = heightval;
+                        heightval = heightval / 100;
+                        heightval = heightval * heightval;
+
+                        var weightval = double.Parse(weight.Text);
+                        var bmi = weightval / heightval;
+
+                        weightval = weightval * 10;
+                        heightforBMR = heightforBMR * 6.25;
+
+                        int ageval = int.Parse(agebmi.Text);
+
+                        ageval = ageval * 5;
+                        double bmr = 0;
+                        
+                        if (genderval == "Female")
+                        {
+                            bmr = weightval + heightforBMR - ageval - 161;
+                        }
+                        else
+                        {
+                            bmr = weightval + heightforBMR - ageval + 5;
+                        }
+
+                        cmd.Parameters.AddWithValue("@BMI", bmi); // Replace bmiValue with the actual value.
+                        cmd.Parameters.AddWithValue("@BMR", bmr); // Replace bmrValue with the actual value.
+                        cmd.Parameters.AddWithValue("@CustomerID", customerIDValue);
+
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Body composition data added successfully");
+                        MainClass.con.Close();
+                        ShowBodyComposition(guna2DataGridView2, idbc, datebc, bcabc, heightbc, weightbc, agebc, fatsbc, proteinbc, waterbc, mineralsbc, visceralfatsbc, abdominalfatsbc, bmibc, bmrbc);
+                        tabControl1.SelectedIndex = 1;
+                    }
+                    catch (Exception ex)
+                    {
+                        MainClass.con.Close();
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("First name, Family name, gender, mobile no, Date of birth are mandory!.");
+                }
+            }
+            else
+            {
+                if (filenobmi.Text != "" )
+                {
+                    try
+                    {
+                        MainClass.con.Open();
+                        SqlCommand cmd = new SqlCommand("UPDATE BodyComposition SET DATE = @DATE, BCA = @BCA, LENGTH = @LENGTH, WEIGHT = @WEIGHT, AGE = @AGE, FATS = @FATS, PROTEIN = @PROTEIN, WATER = @WATER, MINERALS = @MINERALS, VISCERAL_FATS = @VISCERAL_FATS, ABDOMINAL_FAT = @ABDOMINAL_FAT, BMI = @BMI, BMR = @BMR WHERE CustomerID = @CustomerID", MainClass.con);
+
+                        // Assuming you have a customer ID, replace customerIDValue with the actual ID.
+                        int customerIDValue = int.Parse(filenobmi.Text); // Get the actual customer ID from your application logic.
+
+                        // Set parameters for the BodyComposition update
+                        cmd.Parameters.AddWithValue("@DATE", DateTime.Now); // Assuming the current date is used.
+                        cmd.Parameters.AddWithValue("@BCA", bca.Text); // Replace bcaValue with the actual value.
+                        cmd.Parameters.AddWithValue("@LENGTH", height.Text); // Replace lengthValue with the actual value.
+                        cmd.Parameters.AddWithValue("@WEIGHT", weight.Text); // Replace weightValue with the actual value.
+                        cmd.Parameters.AddWithValue("@AGE", agebmi.Text); // Replace ageValue with the actual value.
+                        cmd.Parameters.AddWithValue("@FATS", fats.Text); // Replace fatsValue with the actual value.
+                        cmd.Parameters.AddWithValue("@PROTEIN", protein.Text); // Replace proteinValue with the actual value.
+                        cmd.Parameters.AddWithValue("@WATER", water.Text); // Replace waterValue with the actual value.
+                        cmd.Parameters.AddWithValue("@MINERALS", minerals.Text); // Replace mineralsValue with the actual value.
+                        cmd.Parameters.AddWithValue("@VISCERAL_FATS", visceralfats.Text); // Replace visceralFatsValue with the actual value.
+                        cmd.Parameters.AddWithValue("@ABDOMINAL_FAT", abdominalfats.Text); // Replace abdominalFatValue with the actual value.
+
+                        var genderval = genderbmi.Text;
+                        var heightval = double.Parse(height.Text);
+                        var heightforBMR = heightval;
+                        heightval = heightval / 100;
+                        heightval = heightval * heightval;
+
+                        var weightval = double.Parse(weight.Text);
+                        var bmi = weightval / heightval;
+
+                        weightval = weightval * 10;
+                        heightforBMR = heightforBMR * 6.25;
+
+                        int ageval = int.Parse(agebmi.Text);
+
+                        ageval = ageval * 5;
+                        double bmr = 0;
+
+                        if (genderval == "Female")
+                        {
+                            bmr = weightval + heightforBMR - ageval - 161;
+                        }
+                        else
+                        {
+                            bmr = weightval + heightforBMR - ageval + 5;
+                        }
+
+                        cmd.Parameters.AddWithValue("@BMI", bmi); // Replace bmiValue with the actual value.
+                        cmd.Parameters.AddWithValue("@BMR", bmr); // Replace bmrValue with the actual value.
+                        cmd.Parameters.AddWithValue("@CustomerID", filenoforbmi);
+
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Body composition data updated successfully");
+                        MainClass.con.Close();
+                        bca.Text = "";
+                        height.Text = "";
+                        weight.Text = "";
+                        protein.Text = "";
+                        water.Text = "";
+                        ShowBodyComposition(guna2DataGridView2, idbc, datebc, bcabc, heightbc, weightbc, agebc, fatsbc, proteinbc, waterbc, mineralsbc, visceralfatsbc, abdominalfatsbc, bmibc, bmrbc);
+                        tabControl1.SelectedIndex = 1;
+                    }
+                    catch (Exception ex)
+                    {
+                        MainClass.con.Close();
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("First name, Family name, gender, mobile no, Date of birth are mandory!.");
+                }
+            }
+        }
+        static string filenoforbmi;
+        private void back_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 1;
+        }
+        private void ShowBodyComposition(DataGridView dgv, DataGridViewColumn id, DataGridViewColumn date, DataGridViewColumn bca, DataGridViewColumn height, DataGridViewColumn weight, DataGridViewColumn age, DataGridViewColumn fats, DataGridViewColumn protein, DataGridViewColumn water, DataGridViewColumn mineral, DataGridViewColumn fat1, DataGridViewColumn fat2, DataGridViewColumn bmi, DataGridViewColumn bmr)
+        {
+            SqlCommand cmd;
+            try
+            {
+                MainClass.con.Open();
+
+                cmd = new SqlCommand("SELECT ID,BCA,WATER,MINERALS,DATE,AGE,LENGTH,WEIGHT,FATS,PROTEIN,ABDOMINAL_FAT,VISCERAL_FATS,BMI,BMR FROM BodyComposition WHERE CustomerID = @CUSTOMERID ORDER BY ID", MainClass.con);
+                cmd.Parameters.AddWithValue("@CUSTOMERID", filenobmi.Text); // Replace 'customerIdToFind' with the actual ID you want to find.
+                
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                id.DataPropertyName = dt.Columns["ID"].ToString();
+                date.DataPropertyName = dt.Columns["DATE"].ToString();
+                bca.DataPropertyName = dt.Columns["BCA"].ToString();
+                height.DataPropertyName = dt.Columns["LENGTH"].ToString();
+                weight.DataPropertyName = dt.Columns["WEIGHT"].ToString();
+                age.DataPropertyName = dt.Columns["AGE"].ToString();
+                fats.DataPropertyName = dt.Columns["FATS"].ToString();
+                protein.DataPropertyName = dt.Columns["PROTEIN"].ToString();
+                water.DataPropertyName = dt.Columns["WATER"].ToString();
+                mineral.DataPropertyName = dt.Columns["MINERALS"].ToString();
+                fat1.DataPropertyName = dt.Columns["VISCERAL_FATS"].ToString();
+                fat2.DataPropertyName = dt.Columns["ABDOMINAL_FAT"].ToString();
+                bmi.DataPropertyName = dt.Columns["BMI"].ToString();
+                bmr.DataPropertyName = dt.Columns["BMR"].ToString();
+               
+
+                dgv.DataSource = dt;
+                MainClass.con.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                MainClass.con.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void EditBMI_Click(object sender, EventArgs e)
+        {
+            edit = 1;
+            try
+            {
+                int selectedIndex = guna2DataGridView2.SelectedRows[0].Index;
+                string BMIID = guna2DataGridView2.SelectedRows[0].Cells[0].Value.ToString();
+                filenoforbmi = BMIID;
+                MainClass.con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM BodyComposition WHERE ID = @ID", MainClass.con);
+                cmd.Parameters.AddWithValue("@ID", BMIID); // Replace 'customerIdToFind' with the actual ID you want to find.
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // Set the retrieved data into input boxes
+                        bca.Text = reader["BCA"].ToString();
+                        height.Text = reader["Length"].ToString();
+                        weight.Text = reader["Weight"].ToString();
+                        minerals.Text = reader["Minerals"].ToString();
+                        visceralfats.Text = reader["VISCERAL_FATS"].ToString();
+                        fats.Text = reader["Fats"].ToString();
+                        protein.Text = reader["Protein"].ToString();
+                        water.Text = reader["Water"].ToString();
+                        abdominalfats.Text = reader["ABDOMINAL_FAT"].ToString();
+                    }
+                    tabControl1.SelectedIndex = 2;
+                }
+                else
+                {
+                    MessageBox.Show("Body Composition not found with ID : " + BMIID);
+                }
+                reader.Close();
+                MainClass.con.Close();
+            }
+            catch (Exception ex)
+            {
+                MainClass.con.Close();
+                MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
